9a063ee6692bf37b3f8e9cd85d4b2c0269ed2a08
zarnii <tryakin_gd1@mail.ru>
2024-05-02 17:35:38 +0300

    Фикс бага - Пофикшен баг с неправльным отображением выбранного варианта ответа в тесте.

---
+++ GraphApp/Services/Converters/AnswerGroupNameConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace GraphApp.Services.Converters
+{
+    public class AnswerGroupNameConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var source = (ItemsControl)value;
+
+            return source.DataContext.GetHashCode();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
